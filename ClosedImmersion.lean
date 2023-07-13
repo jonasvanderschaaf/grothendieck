0f2635e3b3ce79a228f4ae731e8d9d723054a73d
jonasvanderschaaf <niet@jonasvanderschaaf.nl>
2023-07-13 13:06:53 +0200

    Prove closed immersions respect isomorphisms

--- ClosedImmersion.lean
+++ ClosedImmersion.lean
@@ -11,11 +11,9 @@ open Topology
 
 namespace AlgebraicGeometry
 
-variable {X Y : Scheme}
-
 -- A closed immersion is a closed immersion of topological spaces which is
 -- surjective on stalks. This definition is due to Mumford.
-class Scheme.IsClosedImmersion (f : X ⟶ Y) : Prop where
+class Scheme.IsClosedImmersion {X Y : Scheme} (f : X ⟶ Y) : Prop where
   base_closed_emb: ClosedEmbedding f.1.base
   surj_on_stalks: ∀ x : X, Function.Surjective (PresheafedSpace.stalkMap f.1 x) 
 
@@ -40,5 +38,30 @@ theorem isClosedImmersion_stableUnderComposition :
       have hf_surj_x := hf_surj x
       have hg_surj_fx := hg_surj (f.val.base x)
       exact hf_surj_x.comp hg_surj_fx
+
+theorem iso_is_closed_immersion {X Y : Scheme} {f: X ⟶ Y} [hf: IsIso f] : Scheme.IsClosedImmersion f := by
+  constructor
+  . have := PresheafedSpace.base_isIso_of_iso f.val
+    let f_top_iso := TopCat.homeoOfIso (asIso f.val.base)
+    exact Homeomorph.closedEmbedding f_top_iso
+
+  . intro x
+    have := PresheafedSpace.stalkMap.isIso f.val x
+    apply @And.right (Function.Injective ↑(PresheafedSpace.stalkMap f.val x)) _
+    apply ConcreteCategory.bijective_of_isIso
+
+-- Now proving the identity is a closed immersion is a one-liner.
+example {X : Scheme} : Scheme.IsClosedImmersion (𝟙 X) := by
+  apply iso_is_closed_immersion
+
+theorem isClosedImmersion_respectsIso :
+  MorphismProperty.RespectsIso @Scheme.IsClosedImmersion := by
+    constructor <;> intro X Y Z e f hf <;> apply isClosedImmersion_stableUnderComposition
+
+    . apply iso_is_closed_immersion
+
+    . assumption
+    assumption
+    exact iso_is_closed_immersion
 
 end AlgebraicGeometry
